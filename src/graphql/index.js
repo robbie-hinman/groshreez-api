f5e7d4b0b4365399389f504be7f8954855354702
Robbie Hinman <robbie.hinman@gmail.com>
2022-03-05 19:26:41 -0800

    added createUser mutation

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -2,8 +2,14 @@ var { buildSchema } = require('graphql');
 var { GraphQLDateTime } = require('graphql-iso-date');
 const models = require('../db');
 // Construct a schema, using GraphQL schema language
+
 var schema = buildSchema(`
   scalar DateTime
+
+  input UserInput {
+    firstName: String!
+    lastName: String!
+  }
 
   type User {
     firstName: String!
@@ -12,23 +18,43 @@ var schema = buildSchema(`
     createdAt: DateTime!
     updatedAt: DateTime!
   }
+
+  type List {
+    listName: String!
+    id: Int!
+    storeName: String
+    fullfilledDate: DateTime
+    createdAt: DateTime!
+    updatedAt: DateTime!
+  }
   
   type Query {
-    hello: String
     users: [User]
     user(id: Int!): User
+    lists: [List]
+    list(id: Int!): List
+  }
+
+  type Mutation {
+    createUser(firstName: String!, lastName: String!): User
   }
 `);
-
+// updateUser(id: Int!, input: UserInput): User
 // The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
   users: async () => await models.User.findAll(),
   user: async (args) => {
-    console.log(args);
     return await models.User.findByPk(args.id);
+  },
+  lists: async () => await models.List.findAll(),
+  list: async (args) => {
+    return await models.List.findByPk(args.id);
+  },
+  createUser: async ({ firstName, lastName }) => {
+    return models.User.create({
+      firstName,
+      lastName,
+    });
   },
   DateTime: GraphQLDateTime,
 };
